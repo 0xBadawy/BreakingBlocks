643c9a766bc3507e7055895d6f2b06add8377857
Mostafa-El-gelany <Mostafa-El-gelany>
2023-12-08 22:25:48 +0200

    modified code

--- project.py
+++ project.py
@@ -1,25 +1,25 @@
 from collections import deque
 import copy
 import random
-check = [[False] * 8 for _ in range(8)]
+sz=8
+check = [[False] * sz for _ in range(sz)]
 newcur = []
 cur = []
 rr=[0,0,1,-1]
 cc=[1,-1,0,0]
-start = [[''] * 8 for _ in range(8)]
-end = ['........'] * 8
+start = [[''] * sz for _ in range(sz)]
+end = ['.'*sz] * sz
 mp = {}
-
 def print_list(v):
-    for i in range(8):
-        for j in range(8):
+    for i in range(sz):
+        for j in range(sz):
             print(v[i][j],end='')
         print(' ')
     print("-------------------------------------")
 
 def finish():
-    for j in range(8):
-        if cur[7][j] != '.':
+    for j in range(sz):
+        if cur[sz-1][j] != '.':
             return False
     return True
 
@@ -30,28 +30,28 @@ def floodfill2(i, j, c):
         check[i][j] = 0
         for k in range(4):
             ii, jj = i + rr[k], j + cc[k]
-            if 0 <= ii < 8 and 0 <= jj < 8:
+            if 0 <= ii < sz and 0 <= jj < sz:
                 floodfill2(ii, jj, c)
 
 def floodfill1(i, j):
     floodfill2(i, j, cur[i][j])
 
 def fall():
-    for i in range(6, -1, -1):
-        for j in range(8):
+    for i in range(sz-2, -1, -1):
+        for j in range(sz):
             if newcur[i + 1][j] == '.' and newcur[i][j] != '.':
                 k = i
-                while k < 7 and newcur[k + 1][j] == '.':
+                while k < sz-1 and newcur[k + 1][j] == '.':
                     newcur[k + 1] = list(newcur[k + 1])  # Convert the string to a list of characters
                     newcur[k + 1][j] = newcur[k][j]  # Reassign the value in the list
                     newcur[k] = list(newcur[k])  # Convert the string to a list of characters
                     newcur[k][j] = '.'  # Reassign the value in the list
                     k += 1
                     # Check if k + 1 is within the valid range
-                    if k + 1 < 8:
+                    if k + 1 < sz:
                         newcur[k + 1] = ''.join(newcur[k + 1])
                     # Check if k is within the valid range
-                    if k < 8:
+                    if k < sz:
                         newcur[k] = ''.join(newcur[k])
 
 
@@ -60,11 +60,12 @@ def fall():
 for i in range(8):
     start[i] = input()
 '''
-for i in range(8):
-    for j in range(8):
-        if random.randint(1,2) % 2 == 0:
+for i in range(sz):
+    for j in range(sz):
+        p=random.randint(1,2) % 2
+        if p == 0:
             start[i][j]='r'
-        else:
+        elif p==1 :
             start[i][j]='b'
 # Using deque for the queue
 q = deque([(start, 0)])
@@ -74,12 +75,12 @@ while q:
     if finish():
         break
 
-    for i in range(8):
-        for j in range(8):
+    for i in range(sz):
+        for j in range(sz):
             check[i][j] = 0 if cur[i][j] == '.' else 1
 
-    for i in range(8):
-        for j in range(8):
+    for i in range(sz):
+        for j in range(sz):
             if check[i][j] == 1:
                 newcur=copy.deepcopy(cur)
                 floodfill1(i,j)
